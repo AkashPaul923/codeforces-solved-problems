5f63a5d599e67aac6b37dce695e139a242bb2315
AkashPaul923 <akashpaul923@gmail.com>
2024-01-31 23:46:26 +0600

    codeforces problem solve

---
+++ 282a.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int a,x=0;
+    cin>>a;
+    string s;
+    for(int i=0;i<a;i++){
+        cin>>s;
+        if(s[1]=='+')
+            x++;
+        else
+            x--;
+    }
+    cout<<x;
+
+}
